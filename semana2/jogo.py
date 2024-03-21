452d01cde06660099906f4661daebbad33fd5122
moimanu <113472007+moimanu@users.noreply.github.com>
2024-03-21 20:26:10 -0300

    Add files via upload

--- semana2/jogo.py
+++ semana2/jogo.py
@@ -1,13 +1,8 @@
-# Restrições para as entradas =>
 numero = int(input())
-while numero>10000 or numero <0:
-    numero = int(input("Valor inválido, digite outro número: "))
 
-# Resolução do problema =>
-def soma (n):
-    if n==1:
-        return 1
-    else: 
-        return n + soma(n-1)
-total = soma(numero+1)
+total = 1
+
+for i in range(numero+1, 1, -1):
+    total = total + i
+    
 print(total)
--- semana2/nota.py
+++ semana2/nota.py
@@ -1,21 +1,15 @@
-# Restrições para as entradas =>
 ent1 = int(input())
-while ent1<0 or ent1>160:
-    ent1 = int(input("Valor inválido, tente outro: "))
 ent2 = int(input())
-while ent2<0 or ent2>160:
-    ent1 = int(input("Valor inválido, tente outro: "))
 
-# Resolução do problema =>
 g = max(ent1,ent2)
 p = min(ent1,ent2)
 h = 70
 b = 160
 aTotal = h*b
-aEsquerda = p*h+(((g-p)**2)/2)
+aEsquerda = (p*h)+((((g-p)*h)/2))
 if aEsquerda == aTotal/2:
     print(0)
-elif aEsquerda < aTotal/2:
+elif aEsquerda > aTotal/2:
     print(1)
 else:
     print(2)
