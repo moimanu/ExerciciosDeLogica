671540bc1b704b6089baf55b524ebd21afd93d6f
moimanu <113472007+moimanu@users.noreply.github.com>
2024-03-22 02:19:44 -0300

    Add files via upload

--- semana2/wifi.py
+++ semana2/wifi.py
@@ -1,16 +1,16 @@
-# Função para transformar a entrada em uma lista com inteiros =>
-def criarLista():
-    entrada = input()
-    listaEntrada = list(map(int, entrada.split()))
-    return listaEntrada
-
-# Entrada de dados =>
 nSalas = int((input()))
 ldl = []
 for n in range(nSalas):
-    ldl.append(criarLista())
+    ldl.append(list(map(int, input().split())))
 
 contador = 0
-
+for x in range(len(ldl)):
+    ehOMenor = True
+    for y in range(len(ldl)):
+        if x!=y:
+            if ldl[x][0]<ldl[y][0] and ldl[x][1]>ldl[y][1] and ldl[x][2]>ldl[y][2] and ldl[x][3]<ldl[y][3]:
+                ehOMenor = False
+                break
+    if ehOMenor:
+        contador = contador + 1
 print(contador)
-print(ldl)
