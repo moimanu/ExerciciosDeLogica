1c3bd2854117f38afe0506c0646ec2139f9ea35e
moimanu <113472007+moimanu@users.noreply.github.com>
2024-03-27 12:50:29 -0300

    Update wifi.py

--- semana2/wifi.py
+++ semana2/wifi.py
@@ -5,7 +5,8 @@ for n in range(int((input()))):
 contador = 0
 for x in range(len(m)):
     for y in range(len(m)):
-            if m[x][0]<m[y][0] and m[x][1]>m[y][1] and m[x][2]>m[y][2] and m[x][3]<m[y][3]:
-                contador = contador + 1
-                break
+        if m[x][0]<m[y][0] and m[x][1]>m[y][1] and m[x][2]>m[y][2] and m[x][3]<m[y][3]:
+            contador = contador + 1
+            break
+            
 print(len(m)-contador)
