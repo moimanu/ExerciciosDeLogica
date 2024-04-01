e5641ccc5d9f15ad5f1296dbb7c454eacc07eac4
moimanu <113472007+moimanu@users.noreply.github.com>
2024-04-01 15:26:33 -0300

    Add files via upload

--- semana3/visita.py
+++ semana3/visita.py
@@ -1,54 +1,51 @@
 entrada1 = list(map(int, input().split()))
 if entrada1[1] > entrada1[2]:
     entrada1 = [entrada1[0],entrada1[2],entrada1[1]]
-qntd_de_entradas = entrada1[0] - 1
-vert_ini = entrada1[1]
-vert__fin = entrada1[2]
+qntdN = entrada1[0] - 1
+cityA = entrada1[1]
+cityB = entrada1[2]
 
 maior_vertice = 0
-
-#Criação de uma matriz com todas as entradas =>
 matriz=[]
-for n in range(qntd_de_entradas):
-    temp = list(map(int, input().split())) # <= Lista temporária do input para adicionar à matriz
-    temp = [temp[0], temp[1], temp[2]]  
-    matriz.append(temp)
-    temp = [temp[1], temp[0], temp[2]]  
-    matriz.append(temp)     
+for n in range(qntdN):
+    temp = list(map(int, input().split())) 
+
+    temp = [temp[0], temp[1], temp[2]]
+    matriz.append(temp)                
+    temp = [temp[1], temp[0], temp[2]] 
+    matriz.append(temp)                
 
     if temp[0]>maior_vertice:
         maior_vertice = temp[0]  
 
-#Criação de uma tabela hash (para o grafo) a partir da matriz =>
 grafo = {}
-for n in matriz:
+for n in matriz: 
     grafo [n[0]] = {}
-for n in matriz:
+for n in matriz: 
     grafo [n[0]] [n[1]] = n[2]
 grafo [maior_vertice] = {}
 
-#Função para encontrar o caminho de "A" até "B"
 processado = []
-lista_final = []
-def procurar_caminho (vert_ini):
-    processado.append(vert_ini)
-
-    for n in grafo[vert_ini].keys(): 
-        if n == vert__fin:
+seq_city = []
+def procurar_caminho (cityA):
+    processado.append(cityA) 
+    for n in grafo[cityA].keys():  
+        if n == cityB:           
             processado.append(n)
             for n in processado:
-                lista_final.append(n)
+                seq_city.append(n) 
             break
-        grafo[n].pop(vert_ini, None) # <= Serve para tirar os vértices cíclicos...
-        procurar_caminho(n)
-        if n != vert__fin:
+
+        grafo[n].pop(cityA, None)
+
+        procurar_caminho(n) 
+
+        if n != cityB:     
             processado.pop()
 
-procurar_caminho(vert_ini)
+procurar_caminho(cityA)
 
-#Somátorio das arestas para encontrar o valor do caminho
 tamanho_caminho = 0
-for n in range(1, len(lista_final)):
-    tamanho_caminho = tamanho_caminho + grafo[lista_final[n-1]] [lista_final[n]]
-
+for n in range(1, len(seq_city)):
+    tamanho_caminho = tamanho_caminho + grafo[seq_city[n-1]] [seq_city[n]]
 print(tamanho_caminho)
---
+++ semana3/visita_comentado.py
@@ -0,0 +1,62 @@
+#PASSO 1)
+#Primeira entrada para definir N, A e B =>
+entrada1 = list(map(int, input().split())) # <= Criação de uma lista com a primeira entrada
+if entrada1[1] > entrada1[2]:
+    entrada1 = [entrada1[0],entrada1[2],entrada1[1]] # <= Ordenando o caminho no sentido da "menor" cidade para a "maior" cidade
+qntdN = entrada1[0] - 1
+cityA = entrada1[1]
+cityB = entrada1[2]
+
+#PASSO 2)
+#Criação de uma matriz em que cada item representa uma aresta "D" que vai do "P" ao "Q" =>
+maior_vertice = 0
+matriz=[]
+for n in range(qntdN):
+    temp = list(map(int, input().split())) # <= Lista temporária do input para adicionar à matriz
+
+    temp = [temp[0], temp[1], temp[2]] #| <= Criando simetria entre todos os vértices
+    matriz.append(temp)                #| 
+    temp = [temp[1], temp[0], temp[2]] #| 
+    matriz.append(temp)                #|
+
+    if temp[0]>maior_vertice: # <= Definindo o maior vértice para declarar o fim do grafo
+        maior_vertice = temp[0]  
+
+#PASSO 3)
+#Criação de uma "tabela hash" (dicionário python), para o grafo, a partir da matriz =>
+grafo = {}
+for n in matriz: # <= Abri chaves para todos os vértices de partida
+    grafo [n[0]] = {}
+for n in matriz: # <= Adicionei os vértices de destino respectivos aos vértices de partida
+    grafo [n[0]] [n[1]] = n[2]
+grafo [maior_vertice] = {} # <= Declarando o fim do grafo
+
+#PASSO 4)
+#Função para encontrar o caminho de "A" até "B"
+processado = []
+seq_city = []
+def procurar_caminho (cityA):
+    processado.append(cityA) # <= Adiciona o vertice aos v. processados
+
+    for n in grafo[cityA].keys():  #| <= Pra cada vértice destino do vértice de partida:
+        if n == cityB:             #| <= Se encontrar a cidade destino
+            processado.append(n)   #| <= Armazenar cidade destino aos v. processados
+            for n in processado:   #| <= Armazenar as cidades processadas na "sequencia final de cidades"
+                seq_city.append(n) #|
+            break                  #| <= Termina
+
+        grafo[n].pop(cityA, None) # <= Serve para tirar os vértices cíclicos...
+
+        procurar_caminho(n) # <= Se não encontrou o fim, volta à função (recursão)
+
+        if n != cityB:       #| <= Se o caminho não levar à nada:
+            processado.pop() #| <= Tira o vértice dos processados e passa para o próximo
+
+procurar_caminho(cityA) # <= Chama a função
+
+#PASSO 5)
+#Somátorio das arestas para encontrar o valor do caminho
+tamanho_caminho = 0
+for n in range(1, len(seq_city)):
+    tamanho_caminho = tamanho_caminho + grafo[seq_city[n-1]] [seq_city[n]]
+print(tamanho_caminho)
