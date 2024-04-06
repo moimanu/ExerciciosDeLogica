acd97c4b5140bd4553f4a4ac84352084f94db05d
moimanu <113472007+moimanu@users.noreply.github.com>
2024-04-06 01:30:01 -0300

    Add files via upload

--- semana4/chuva.py
+++ semana4/chuva.py
@@ -6,26 +6,23 @@ matriz = []
 for n in range(linhas): # <= Definindo a segunda entrada
     matriz.append(list(input()))
 
-def bloco_abaixo(): # <= Função para conferir se há bloco abaixo da gota
-    for i in range(1, linhas):
-        for j in range(colunas):
-            if matriz[i-1][j] == "o" and matriz[i][j] == ".":
-                matriz[i][j] = "o"
-                gota_acima()
-
 def gota_acima(): # <= Função para conferir gota em cima de um bloco
-    for i in range(1, linhas):
-        for j in range(colunas):
-            if matriz[i-1][j] == "o" and matriz[i][j] == "#":
-                if j != 0 or j != len(colunas):
-                    if matriz[i-1][j-1] != "#":
-                        matriz[i-1][j-1] = "o"
-                    if matriz[i-1][j+1] != "#":
-                        matriz[i-1][j+1] = "o"
-                    bloco_abaixo()
+    if matriz[i-1][j] == "o" and matriz[i][j] == "#":
+        if j != 0 or j != len(colunas):
+            if matriz[i-1][j-1] != "#":
+                matriz[i-1][j-1] = "o"
+            if matriz[i-1][j+1] != "#":
+                matriz[i-1][j+1] = "o"
 
-gota_acima()
-bloco_abaixo()
+def bloco_abaixo(): # <= Função para conferir se há bloco abaixo da gota
+    if matriz[i-1][j] == "o" and matriz[i][j] == ".":
+        matriz[i][j] = "o"
+        gota_acima()
 
 for linha in matriz: # <= Definindo a saída
+    for coluna in range(colunas): # <= Conferindo as linhas "n" vezes, em que "n" representa o número de colunas
+        for i in range(1, linhas):
+            for j in range(colunas):
+                gota_acima()
+                bloco_abaixo()
     print(''.join(linha))
