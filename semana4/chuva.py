96c743b96e753414dab86c1a685ad01f0e4f0e2d
moimanu <113472007+moimanu@users.noreply.github.com>
2024-04-06 13:06:13 -0300

    Update chuva.py

--- semana4/chuva.py
+++ semana4/chuva.py
@@ -15,10 +15,11 @@ def bloco_abaixo(): # <= Função para conferir se há bloco abaixo da gota
     if matriz[i-1][j] == "o" and matriz[i][j] == ".":
         matriz[i][j] = "o"
 
+for coluna in range(colunas): # <= Conferindo as linhas "n" vezes, em que "n" representa o número de colunas
+    for i in range(1, linhas):
+        for j in range(colunas):
+            gota_acima()
+            bloco_abaixo()
+
 for linha in matriz: # <= Definindo a saída
-    for coluna in range(colunas): # <= Conferindo as linhas "n" vezes, em que "n" representa o número de colunas
-        for i in range(1, linhas):
-            for j in range(colunas):
-                gota_acima()
-                bloco_abaixo()
     print(''.join(linha))
