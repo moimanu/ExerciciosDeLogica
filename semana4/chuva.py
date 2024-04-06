01016fc4b804931d001f2e93b2ecc85021ddbb29
moimanu <113472007+moimanu@users.noreply.github.com>
2024-04-06 01:42:16 -0300

    Add files via upload

--- semana4/chuva.py
+++ semana4/chuva.py
@@ -8,16 +8,12 @@ for n in range(linhas): # <= Definindo a segunda entrada
 
 def gota_acima(): # <= Função para conferir gota em cima de um bloco
     if matriz[i-1][j] == "o" and matriz[i][j] == "#":
-        if j != 0 or j != len(colunas):
-            if matriz[i-1][j-1] != "#":
-                matriz[i-1][j-1] = "o"
-            if matriz[i-1][j+1] != "#":
-                matriz[i-1][j+1] = "o"
+        matriz[i-1][j-1] = "o"
+        matriz[i-1][j+1] = "o"
 
 def bloco_abaixo(): # <= Função para conferir se há bloco abaixo da gota
     if matriz[i-1][j] == "o" and matriz[i][j] == ".":
         matriz[i][j] = "o"
-        gota_acima()
 
 for linha in matriz: # <= Definindo a saída
     for coluna in range(colunas): # <= Conferindo as linhas "n" vezes, em que "n" representa o número de colunas
