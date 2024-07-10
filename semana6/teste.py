536a60a7560004919f1973ebf6f20dc4aac4e314
Moisés Emanuel <113472007+moimanu@users.noreply.github.com>
2024-07-10 18:55:42 -0300

    Add files via upload

---
+++ semana6/teste.py
@@ -0,0 +1,43 @@
+entrada1 = list(map(int, input().split())) # <= Primeira entrada (E, I, V)
+
+lista=[]
+for n in range(entrada1[1]):
+    lista.append(list(map(int, input().split()))) # <= lista com os pontos A e B
+
+ultimaEntrada = list(map(int, input().split())) # <= Última entrada (Xi)
+
+print("LISTA: ", lista)
+
+grafo = {}
+for n in lista:
+    grafo[n[1]] = []
+for n in lista:
+    grafo[n[1]].append(n[0])
+    
+print("GRAFO: ", grafo)
+
+listaConexoesDiretas = []
+def conferirConexaoDireta(vertice):
+    if (grafo.get(vertice, False)):
+        ultimaEntrada[0] = grafo[vertice]
+        listaConexoesDiretas.append(grafo[vertice])
+        conferirConexaoDireta(grafo[vertice])
+print("Conexões diretas: ", listaConexoesDiretas)
+
+conferirConexaoDireta(ultimaEntrada[0])
+
+listaDeUltimosVertices = []
+def printarGrafo(vertice):
+    if (grafo.get(vertice, False)):
+        for n in grafo[vertice]:
+            print(n)
+            printarGrafo(n)
+    else: 
+        print("O último vértice foi: ", vertice)
+        listaDeUltimosVertices.append(vertice)
+        
+printarGrafo(ultimaEntrada[0])
+
+print("FILHOS DO PRIMEIRO VÉRTICE: ", grafo[ultimaEntrada[0]])
+print("LISTA DE ÚLTIMOS VÉRTICES: ",listaDeUltimosVertices)
+print("ÚLTIMA ENTRADA: ", ultimaEntrada)
